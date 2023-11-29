8f16ad52c266447fb0d7eb25b389c2c51f6135e5
simon-k1993 <kozoloski_simon@yahoo.com>
2023-11-29 01:08:11 +0100

    DataAccess/Domain/DTOs/Mappers/Specifications

---
+++ .Net+Angular E-CommerceApp/WebApi/TheShop/TheShop.DataAccess/Data/SpecificationEvaluator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheShop.Domain.Entities;
+using TheShop.Specifications;
+
+namespace TheShop.DataAccess.Data
+{
+    public class SpecificationEvaluator<TEntity> where TEntity : BaseEntity
+    {
+        public static IQueryable<TEntity> GetQuery(IQueryable<TEntity> inputQuery, ISpecification<TEntity> spec)
+        {
+            var query = inputQuery;
+
+            if (spec.Criteria != null)
+            {
+                query = query.Where(spec.Criteria);
+            }          
+
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+            return query;
+        }
+    }
+}
